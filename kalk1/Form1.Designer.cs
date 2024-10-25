d3e658c171e828ab690443e59267a2bd9c3c7c31
3668841 <3668841@TE-GDA-D-D21E14>
2024-10-25 15:31:28 +0200

    Dodaj pliki projektów.

---
+++ kalk1/Form1.Designer.cs
@@ -0,0 +1,338 @@
+﻿namespace kalk1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            tableLayoutPanel2 = new TableLayoutPanel();
+            button1 = new Button();
+            button2 = new Button();
+            tableLayoutPanel3 = new TableLayoutPanel();
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            button3 = new Button();
+            button4 = new Button();
+            tableLayoutPanel4 = new TableLayoutPanel();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            button12 = new Button();
+            button13 = new Button();
+            button14 = new Button();
+            button15 = new Button();
+            button16 = new Button();
+            tableLayoutPanel1.SuspendLayout();
+            tableLayoutPanel2.SuspendLayout();
+            tableLayoutPanel3.SuspendLayout();
+            flowLayoutPanel1.SuspendLayout();
+            tableLayoutPanel4.SuspendLayout();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(152, 83);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(284, 23);
+            textBox1.TabIndex = 0;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 2;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.Controls.Add(button11, 0, 1);
+            tableLayoutPanel1.Controls.Add(button12, 1, 1);
+            tableLayoutPanel1.Controls.Add(button15, 1, 0);
+            tableLayoutPanel1.Controls.Add(button16, 0, 0);
+            tableLayoutPanel1.Location = new Point(152, 112);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 2;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.Size = new Size(127, 100);
+            tableLayoutPanel1.TabIndex = 1;
+            // 
+            // tableLayoutPanel2
+            // 
+            tableLayoutPanel2.ColumnCount = 2;
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel2.Controls.Add(button2, 0, 0);
+            tableLayoutPanel2.Controls.Add(button1, 1, 1);
+            tableLayoutPanel2.Controls.Add(button13, 0, 1);
+            tableLayoutPanel2.Controls.Add(button14, 1, 0);
+            tableLayoutPanel2.Location = new Point(285, 112);
+            tableLayoutPanel2.Name = "tableLayoutPanel2";
+            tableLayoutPanel2.RowCount = 2;
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel2.Size = new Size(151, 100);
+            tableLayoutPanel2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(78, 53);
+            button1.Name = "button1";
+            button1.Size = new Size(69, 44);
+            button1.TabIndex = 1;
+            button1.Text = "x";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(3, 3);
+            button2.Name = "button2";
+            button2.Size = new Size(69, 44);
+            button2.TabIndex = 2;
+            button2.Text = "c";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel3
+            // 
+            tableLayoutPanel3.ColumnCount = 2;
+            tableLayoutPanel3.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel3.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel3.Controls.Add(flowLayoutPanel1, 0, 0);
+            tableLayoutPanel3.Controls.Add(button3, 0, 1);
+            tableLayoutPanel3.Controls.Add(button4, 1, 1);
+            tableLayoutPanel3.Controls.Add(button7, 1, 0);
+            tableLayoutPanel3.Location = new Point(152, 218);
+            tableLayoutPanel3.Name = "tableLayoutPanel3";
+            tableLayoutPanel3.RowCount = 2;
+            tableLayoutPanel3.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel3.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel3.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel3.Size = new Size(127, 100);
+            tableLayoutPanel3.TabIndex = 3;
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.Controls.Add(button6);
+            flowLayoutPanel1.Location = new Point(3, 3);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(57, 44);
+            flowLayoutPanel1.TabIndex = 1;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(3, 53);
+            button3.Name = "button3";
+            button3.Size = new Size(57, 44);
+            button3.TabIndex = 3;
+            button3.Text = "1";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(66, 53);
+            button4.Name = "button4";
+            button4.Size = new Size(58, 44);
+            button4.TabIndex = 4;
+            button4.Text = "2";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel4
+            // 
+            tableLayoutPanel4.ColumnCount = 2;
+            tableLayoutPanel4.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel4.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel4.Controls.Add(button5, 0, 1);
+            tableLayoutPanel4.Controls.Add(button8, 1, 1);
+            tableLayoutPanel4.Controls.Add(button9, 0, 0);
+            tableLayoutPanel4.Controls.Add(button10, 1, 0);
+            tableLayoutPanel4.Location = new Point(285, 218);
+            tableLayoutPanel4.Name = "tableLayoutPanel4";
+            tableLayoutPanel4.RowCount = 2;
+            tableLayoutPanel4.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel4.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel4.Size = new Size(151, 100);
+            tableLayoutPanel4.TabIndex = 0;
+            tableLayoutPanel4.Paint += tableLayoutPanel4_Paint;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(3, 53);
+            button5.Name = "button5";
+            button5.Size = new Size(69, 44);
+            button5.TabIndex = 0;
+            button5.Text = "3";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(3, 3);
+            button6.Name = "button6";
+            button6.Size = new Size(54, 41);
+            button6.TabIndex = 0;
+            button6.Text = "4";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(66, 3);
+            button7.Name = "button7";
+            button7.Size = new Size(58, 44);
+            button7.TabIndex = 5;
+            button7.Text = "5";
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(78, 53);
+            button8.Name = "button8";
+            button8.Size = new Size(70, 44);
+            button8.TabIndex = 1;
+            button8.Text = "=";
+            button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            button9.Location = new Point(3, 3);
+            button9.Name = "button9";
+            button9.Size = new Size(69, 44);
+            button9.TabIndex = 2;
+            button9.Text = "6";
+            button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            button10.Location = new Point(78, 3);
+            button10.Name = "button10";
+            button10.Size = new Size(70, 44);
+            button10.TabIndex = 3;
+            button10.Text = "-";
+            button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            button11.Location = new Point(3, 53);
+            button11.Name = "button11";
+            button11.Size = new Size(57, 44);
+            button11.TabIndex = 0;
+            button11.Text = "7";
+            button11.UseVisualStyleBackColor = true;
+            // 
+            // button12
+            // 
+            button12.Location = new Point(66, 53);
+            button12.Name = "button12";
+            button12.Size = new Size(58, 44);
+            button12.TabIndex = 1;
+            button12.Text = "8";
+            button12.UseVisualStyleBackColor = true;
+            // 
+            // button13
+            // 
+            button13.Location = new Point(3, 53);
+            button13.Name = "button13";
+            button13.Size = new Size(69, 44);
+            button13.TabIndex = 3;
+            button13.Text = "9";
+            button13.UseVisualStyleBackColor = true;
+            // 
+            // button14
+            // 
+            button14.Location = new Point(78, 3);
+            button14.Name = "button14";
+            button14.Size = new Size(70, 44);
+            button14.TabIndex = 4;
+            button14.Text = ":";
+            button14.UseVisualStyleBackColor = true;
+            // 
+            // button15
+            // 
+            button15.Location = new Point(66, 3);
+            button15.Name = "button15";
+            button15.Size = new Size(58, 44);
+            button15.TabIndex = 2;
+            button15.Text = "0";
+            button15.UseVisualStyleBackColor = true;
+            // 
+            // button16
+            // 
+            button16.Location = new Point(3, 3);
+            button16.Name = "button16";
+            button16.Size = new Size(57, 44);
+            button16.TabIndex = 3;
+            button16.Text = "CE";
+            button16.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(607, 450);
+            Controls.Add(tableLayoutPanel4);
+            Controls.Add(tableLayoutPanel3);
+            Controls.Add(tableLayoutPanel2);
+            Controls.Add(tableLayoutPanel1);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            RightToLeftLayout = true;
+            Text = "Form1";
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel2.ResumeLayout(false);
+            tableLayoutPanel3.ResumeLayout(false);
+            flowLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel4.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private TableLayoutPanel tableLayoutPanel1;
+        private TableLayoutPanel tableLayoutPanel2;
+        private TableLayoutPanel tableLayoutPanel3;
+        private TableLayoutPanel tableLayoutPanel4;
+        private FlowLayoutPanel flowLayoutPanel1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button11;
+        private Button button12;
+        private Button button15;
+        private Button button16;
+        private Button button13;
+        private Button button14;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+    }
+}
---
+++ kalk1/Form1.cs
@@ -0,0 +1,30 @@
+namespace kalk1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel4_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
